be79ee4e70dca674f1850b02b6d10b3cb9dbeaf3
Marc Wiedermann <marcwiedermann@posteo.de>
2018-01-21 16:08:00 +0100

    Google scholar done

--- googlescholar.py
+++ googlescholar.py
@@ -1,6 +1,7 @@
 import lxml.html
 import requests
 import time
+import client_example
 
 def get_citation_count():
     req = requests.get("https://scholar.google.de/citations?user=3SsX1ecAAAAJ")
@@ -9,13 +10,45 @@ def get_citation_count():
     value = int(stream.find_class("gsc_rsb_std")[0].text)
     return value
 
-current_count = get_citation_count()
 
-while True:
-    next_count = get_citation_count()
-    if next_count != current_count:
-        print(next_count)
-        current_count = next_count
+c = client_example.Communicator()
+
+try:
+    try:
+        with open("michis_score.txt", "r") as f:
+            current_score,last_status,days_on= f.read().split(",")
+            current_score = int(current_score)
+            days_on = int(days_on)
+    except:
+        current_score = get_citation_count()
+        last_status = "OFF" 
+        days_on = 0
+
+    next_score = get_citation_count()
+
+    if next_score != current_score:
+        c.send_msg("7,FAST")
+        current_score = next_score
+        last_status = "FAST"
+        days_on = 0
+    elif last_status == "FAST":
+        c.send_msg("7,SLOW")
+        last_status = "SLOW"
+        days_on = 0
+    elif last_status == "SLOW":
+        last_status = "ON"
+        c.send_msg("7,ON")
+        days_on += 1
+    elif last_status == "ON" and days_on < 5:
+        c.send_msg("7,ON")
+        days_on += 1
     else:
-        print("no change")
-    time.sleep(60*60)
+        c.send_msg("7,OFF")
+        last_status = "OFF"
+        days_on = 0
+
+    with open("michis_score.txt", "w") as f:
+        outstring = "{0},{1},{2}".format(current_score,last_status,days_on)
+        f.write(outstring)
+except:
+    c.send_msg("7,ERROR")
